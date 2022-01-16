e6da62d0ebd181bee49b7e4170618fcd3697a17a
Pual Zhang <zsb940680883@outlook.com>
2022-01-17 00:07:44 +0800

    add some layouts with vanitygaps

--- config.h
+++ config.h
@@ -59,11 +59,26 @@ static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
+#define FORCE_VSPLIT 1  /* nrowgrid layout: force two clients to always split vertically */
+#include "vanitygaps.c"
+
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "Tile",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ "Tile",     tile },    /* first entry is default */
 	{ "[M]",      monocle },
+	{ "[@]",      spiral },
+	{ "[\\]",     dwindle },
+	{ "H[]",      deck },
+	{ "TTT",      bstack },
+	{ "===",      bstackhoriz },
+	{ "HHH",      grid },
+	{ "###",      nrowgrid },
+	{ "---",      horizgrid },
+	{ ":::",      gaplessgrid },
+	{ "|M|",      centeredmaster },
+	{ ">M>",      centeredfloatingmaster },
+	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ NULL,       NULL },
 };
 
 /* key definitions */
@@ -93,49 +108,57 @@ static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "90x30", NULL };
 
 static Key keys[] = {
-	/* modifier            key                      function        argument */
-	{ MODKEY,              XK_d,                    spawn,          {.v = dmenucmd } },
-	{ MODKEY,              XK_Return,               spawn,          {.v = termcmd } },
-  { MODKEY,              XK_c,                    spawn,          {.v = browsercmd } },
-	{ 0,                   XF86XK_AudioLowerVolume, spawn,          {.v = downvol } },
-	{ 0,                   XF86XK_AudioMute,        spawn,          {.v = mutevol } },
-	{ 0,                   XF86XK_AudioRaiseVolume, spawn,          {.v = upvol   } },
-	{ MODKEY,              XK_bracketleft,          spawn,          {.v = downvol } },
-	{ MODKEY,              XK_backslash,            spawn,          {.v = mutevol } },
-	{ MODKEY,              XK_bracketright,         spawn,          {.v = upvol   } },
-	{ MODKEY|ControlMask,  XK_v,                    spawn,          {.v = start_vpn   } },
-	{ MODKEY|ShiftMask,    XK_j,                    rotatestack,    {.i = +1 } },
-	{ MODKEY|ShiftMask,    XK_k,                    rotatestack,    {.i = -1 } },
-	{ MODKEY,              XK_j,                    focusstack,     {.i = +1 } },
-	{ MODKEY,              XK_k,                    focusstack,     {.i = -1 } },
-	{ MODKEY,              XK_h,                    viewtoleft,     {0} },
-	{ MODKEY,              XK_l,                    viewtoright,    {0} },
-	{ MODKEY|ShiftMask,    XK_h,                    tagtoleft,      {0} },
-	{ MODKEY|ShiftMask,    XK_l,                    tagtoright,     {0} },
-	{ MODKEY|ShiftMask,    XK_u,                    incnmaster,     {.i = +1 } },
-	{ MODKEY|ShiftMask,    XK_i,                    incnmaster,     {.i = -1 } },
-	{ MODKEY,              XK_u,                    setmfact,       {.f = -0.05 } },
-	{ MODKEY,              XK_i,                    setmfact,       {.f = +0.05 } },
-	{ MODKEY,              XK_e,                    hidewin,        {0} },
-	{ MODKEY|ShiftMask,    XK_e,                    restorewin,     {0} },
-	{ MODKEY,              XK_o,                    hideotherwins,  {0}},
-	{ MODKEY|ShiftMask,    XK_o,                    restoreotherwins, {0}},
-	{ MODKEY|ShiftMask,    XK_Return,               zoom,           {0} },
-	{ MODKEY,              XK_Tab,                  view,           {0} },
-	{ MODKEY,              XK_q,                    killclient,     {0} },
-	{ MODKEY,              XK_t,                    setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,              XK_r,                    setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,              XK_y,                    setlayout,      {.v = &layouts[2]} },
-	{ MODKEY|ShiftMask,    XK_f,                    fullscreen,     {0} },
-	{ MODKEY,              XK_space,                setlayout,      {0} },
-	{ MODKEY|ShiftMask,    XK_space,                togglefloating, {0} },
-	{ MODKEY,              XK_f,                    togglescratch,  {.v = scratchpadcmd } },
-	{ MODKEY,              XK_0,                    view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,    XK_0,                    tag,            {.ui = ~0 } },
-	{ MODKEY,              XK_comma,                focusmon,       {.i = -1 } },
-	{ MODKEY,              XK_period,               focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,    XK_comma,                tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,    XK_period,               tagmon,         {.i = +1 } },
+	/* modifier            key                      function            argument */
+	{ MODKEY,              XK_d,                    spawn,              {.v = dmenucmd } },
+	{ MODKEY,              XK_Return,               spawn,              {.v = termcmd } },
+  { MODKEY,              XK_c,                    spawn,              {.v = browsercmd } },
+	{ 0,                   XF86XK_AudioLowerVolume, spawn,              {.v = downvol } },
+	{ 0,                   XF86XK_AudioMute,        spawn,              {.v = mutevol } },
+	{ 0,                   XF86XK_AudioRaiseVolume, spawn,              {.v = upvol   } },
+	{ MODKEY,              XK_bracketleft,          spawn,              {.v = downvol } },
+	{ MODKEY,              XK_backslash,            spawn,              {.v = mutevol } },
+	{ MODKEY,              XK_bracketright,         spawn,              {.v = upvol   } },
+	{ MODKEY|ControlMask,  XK_v,                    spawn,              {.v = start_vpn   } },
+	{ MODKEY|ShiftMask,    XK_j,                    rotatestack,        {.i = +1 } },
+	{ MODKEY|ShiftMask,    XK_k,                    rotatestack,        {.i = -1 } },
+	{ MODKEY,              XK_j,                    focusstack,         {.i = +1 } },
+	{ MODKEY,              XK_k,                    focusstack,         {.i = -1 } },
+	{ MODKEY,              XK_h,                    viewtoleft,         {0} },
+	{ MODKEY,              XK_l,                    viewtoright,        {0} },
+	{ MODKEY|ShiftMask,    XK_h,                    tagtoleft,          {0} },
+	{ MODKEY|ShiftMask,    XK_l,                    tagtoright,         {0} },
+	{ MODKEY|ShiftMask,    XK_u,                    incnmaster,         {.i = +1 } },
+	{ MODKEY|ShiftMask,    XK_i,                    incnmaster,         {.i = -1 } },
+	{ MODKEY,              XK_u,                    setmfact,           {.f = -0.05 } },
+	{ MODKEY,              XK_i,                    setmfact,           {.f = +0.05 } },
+  { MODKEY|ControlMask,  XK_u,                    setcfact,           {.f = -0.05} },
+  { MODKEY|ControlMask,  XK_i,                    setcfact,           {.f = +0.05} },
+  { MODKEY|ControlMask,  XK_o,                    setcfact,           {.f =  0.00} },
+	{ MODKEY,              XK_e,                    hidewin,            {0} },
+	{ MODKEY|ShiftMask,    XK_e,                    restorewin,         {0} },
+	{ MODKEY,              XK_o,                    hideotherwins,      {0}},
+	{ MODKEY|ShiftMask,    XK_o,                    restoreotherwins,   {0}},
+	{ MODKEY|ShiftMask,    XK_Return,               zoom,               {0} },
+	{ MODKEY,              XK_Tab,                  view,               {0} },
+	{ MODKEY,              XK_q,                    killclient,         {0} },
+	{ MODKEY|ShiftMask,    XK_t,                    setlayout,          {.v = &layouts[0]} }, /* Tile */
+	{ MODKEY|ShiftMask,    XK_y,                    setlayout,          {.v = &layouts[2]} }, /* spiral */
+	{ MODKEY|ShiftMask,    XK_d,                    setlayout,          {.v = &layouts[4]} }, /* deck */
+	{ MODKEY|ShiftMask,    XK_b,                    setlayout,          {.v = &layouts[7]} }, /* grid */
+	{ MODKEY|ShiftMask,    XK_g,                    setlayout,          {.v = &layouts[8]} }, /* nrowgrid */
+	{ MODKEY|ShiftMask,    XK_r,                    setlayout,          {.v = &layouts[10]} }, /* gaplessgrid */
+	{ MODKEY|ShiftMask,    XK_f,                    setlayout,          {.v = &layouts[11]} }, /* centeredmaster  */
+	{ MODKEY|ShiftMask,    XK_c,                    setlayout,          {.v = &layouts[12]} }, /* centeredfloatingmaster */
+	{ MODKEY,              XK_f,                    fullscreen,         {0} },
+	{ MODKEY,              XK_space,                setlayout,          {0} },
+	{ MODKEY|ShiftMask,    XK_space,                togglefloating,     {0} },
+	{ MODKEY,              XK_grave,                togglescratch,      {.v = scratchpadcmd } },
+	{ MODKEY,              XK_0,                    view,               {.ui = ~0 } },
+	{ MODKEY|ShiftMask,    XK_0,                    tag,                {.ui = ~0 } },
+	{ MODKEY,              XK_comma,                focusmon,           {.i = -1 } },
+	{ MODKEY,              XK_period,               focusmon,           {.i = +1 } },
+	{ MODKEY|ShiftMask,    XK_comma,                tagmon,             {.i = -1 } },
+	{ MODKEY|ShiftMask,    XK_period,               tagmon,             {.i = +1 } },
 	TAGKEYS(               XK_1,                      0)
 	TAGKEYS(               XK_2,                      1)
 	TAGKEYS(               XK_3,                      2)
@@ -145,7 +168,7 @@ static Key keys[] = {
 	TAGKEYS(               XK_7,                      6)
 	TAGKEYS(               XK_8,                      7)
 	TAGKEYS(               XK_9,                      8)
-	{ MODKEY|ControlMask,  XK_q,      quit,           {0} },
+	{ MODKEY|ControlMask,  XK_q,                    quit,               {0} },
 };
 
 /* button definitions */
--- dwm.c
+++ dwm.c
@@ -108,6 +108,7 @@ typedef struct Client Client;
 struct Client {
 	char name[256];
 	float mina, maxa;
+	float cfact;
 	int x, y, w, h;
 	int oldx, oldy, oldw, oldh;
 	int basew, baseh, incw, inch, maxw, maxh, minw, minh;
@@ -246,6 +247,7 @@ static void setfocus(Client *c);
 static void setfullscreen(Client *c, int fullscreen);
 static void fullscreen(const Arg *arg);
 static void setlayout(const Arg *arg);
+static void setcfact(const Arg *arg);
 static void setmfact(const Arg *arg);
 static void setup(void);
 static void seturgent(Client *c, int urg);
@@ -258,7 +260,6 @@ static void tag(const Arg *arg);
 static void tagmon(const Arg *arg);
 static void tagtoleft(const Arg *arg);
 static void tagtoright(const Arg *arg);
-static void tile(Monitor *);
 static void togglebar(const Arg *arg);
 static void togglefloating(const Arg *arg);
 static void togglescratch(const Arg *arg);
@@ -305,7 +306,6 @@ static char stext[256];
 static int screen;
 static int sw, sh;           /* X display screen geometry width, height */
 static int bh, blw = 0;      /* bar geometry */
-static int enablegaps = 1;   /* enables gaps, used by togglegaps */
 static int lrpad;            /* sum of left and right padding for text */
 static int (*xerrorxlib)(Display *, XErrorEvent *);
 static unsigned int numlockmask = 0;
@@ -1309,6 +1309,7 @@ manage(Window w, XWindowAttributes *wa)
 	c->w = c->oldw = wa->width;
 	c->h = c->oldh = wa->height;
 	c->oldbw = wa->border_width;
+	c->cfact = 1.0;
 
 	updatetitle(c);
 	if (XGetTransientForHint(dpy, w, &trans) && (t = wintoclient(trans))) {
@@ -1941,6 +1942,24 @@ setlayout(const Arg *arg)
 		drawbar(selmon);
 }
 
+void
+setcfact(const Arg *arg) {
+	float f;
+	Client *c;
+
+	c = selmon->sel;
+
+	if(!arg || !c || !selmon->lt[selmon->sellt]->arrange)
+		return;
+	f = arg->f + c->cfact;
+	if(arg->f == 0.0)
+		f = 1.0;
+	else if(f < 0.25 || f > 4.0)
+		return;
+	c->cfact = f;
+	arrange(selmon);
+}
+
 /* arg > 1.0 will set mfact absolutely */
 void
 setmfact(const Arg *arg)
@@ -2122,40 +2141,6 @@ tagmon(const Arg *arg)
 	if (!selmon->sel || !mons->next)
 		return;
 	sendmon(selmon->sel, dirtomon(arg->i));
-}
-
-void
-tile(Monitor *m)
-{
-	unsigned int i, n, h, r, oe = enablegaps, ie = enablegaps, mw, my, ty;
-	Client *c;
-
-	for (n = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), n++);
-	if (n == 0)
-		return;
-
-	if (smartgaps == n) {
-		oe = 0; // outer gaps disabled
-	}
-
-	if (n > m->nmaster)
-		mw = m->nmaster ? (m->ww + m->gappiv*ie) * m->mfact : 0;
-	else
-		mw = m->ww - 2*m->gappov*oe + m->gappiv*ie;
-	for (i = 0, my = ty = m->gappoh*oe, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++)
-		if (i < m->nmaster) {
-			r = MIN(n, m->nmaster) - i;
-			h = (m->wh - my - m->gappoh*oe - m->gappih*ie * (r - 1)) / r;
-			resize(c, m->wx + m->gappov*oe, m->wy + my, mw - (2*c->bw) - m->gappiv*ie, h - (2*c->bw), 0);
-			if (my + HEIGHT(c) + m->gappih*ie < m->wh)
-        my += HEIGHT(c) + m->gappih*ie;
-		} else {
-			r = n - i;
-			h = (m->wh - ty - m->gappoh*oe - m->gappih*ie * (r - 1)) / r;
-			resize(c, m->wx + mw + m->gappov*oe, m->wy + ty, m->ww - mw - (2*c->bw) - 2*m->gappov*oe, h - (2*c->bw), 0);
-			if (ty + HEIGHT(c) + m->gappih*ie < m->wh)
-				ty += HEIGHT(c) + m->gappih*ie;
-		}
 }
 
 void
